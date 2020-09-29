c7be6ea68a4de2a57fd9f589f4d3e3896ae5a4c6
emerson <emersonl.lourenzi@gmail.com>
2020-09-28 21:11:12 -0300

    organizado data e hora da calculadora para atualizar automaticamente.

--- _scripts/controller/CalcController.js
+++ _scripts/controller/CalcController.js
@@ -1,33 +1,63 @@
 class CalcController {
 
     constructor() {
-        this._displayCalc = "0";
+        this._locale = "pt-BR";
+        this._displayCalcEl = document.querySelector("#display");
+        this._dateEl = document.querySelector("#date");
+        this._hourEl = document.querySelector("#hour");
         this._currentDate;
         this.initialize();
     }
 
     initialize() {
 
-        let displayCalcEl = document.querySelector("#display");
-        let dateEl = document.querySelector("#date");
-        let horaEl = document.querySelector("#hour");
+        setInterval(() => {
+            this._hourEl.innerHTML = new Date().toLocaleTimeString(this._locale);
+            this._dateEl.innerHTML = new Date().toLocaleDateString(this._locale, {
+                day: "2-digit",
+                month: "long",
+                year: "numeric"
+            });
+        }, true);
 
-        displayCalcEl.innerHTML = "1234556";
-        dateEl.innerHTML = new Date().toLocaleDateString();
-        horaEl.innerHTML = new Date().toLocaleTimeString();
+        setTimeout(() => {
+            alert(`ACESSE: \nhttp://curriculo.emerson.lourenzi.net`);
+        }, 2000);
 
+        setTimeout(() => {
+            if (window.confirm('Acesse a page')) {
+                window.open('http://curriculo.emerson.lourenzi.net', '_blank');
+            }
+        }, 10000);
+
+    }
+
+    get displayDate() {
+        return this._dateEl.innerHTML;
+    }
+
+    set displayDate(value) {
+        this._dateEl.innerHTML = value;
+    }
+
+    get displayTime() {
+        return this._hourEl;
+    }
+
+    set displayTime(value) {
+        this._hourEl = value;
     }
 
     get displayCalc() {
-        return this._displayCalc;
+        return this._displayCalcEl.innerHTML;
     }
 
     set displayCalc(value) {
-        this._displayCalc = value;
+        this._displayCalcEl.innerHTML = value;
     }
 
     get currentDate() {
-        return this._currentDate;
+        return new Date().toLocaleTimeString("pt-BR");
     }
 
     set currentDate(value) {
